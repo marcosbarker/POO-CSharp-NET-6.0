c528c4337cd3e2c81285ed24329a26d197b64c86
marcosbarker <marcos_bareker@hotmail.com>
2022-04-08 20:15:44 -0300

    code: metodBasic

--- AgenciaBancaria/Models/ContaBancaria.cs
+++ AgenciaBancaria/Models/ContaBancaria.cs
@@ -2,15 +2,68 @@ namespace AgenciaBancaria.Models
 {
     public class ContaBancaria
     {
+        #region Atributos
         public Titular Titular {get; set;}
         public double Saldo {get; private set;}
         public DateTime DataAbertura {get; private set;}
+        private readonly double VALOR_MINIMO = 10.0;
 
+        #endregion
+
+        #region Construtores
         public ContaBancaria(Titular titular, double saldoAbertura)
         {
             Titular = titular;
             Saldo = saldoAbertura;
             DataAbertura = DateTime.Now;
         }
+        
+        #endregion
+    
+        #region Metodos
+
+        public void Depositar(double valor)
+        {
+            if(valor < VALOR_MINIMO)
+            {
+                throw new Exception("O valor minimo para deposito é R$" + VALOR_MINIMO);
+            }
+
+            Saldo += valor;
+        }
+
+        public double Sacar(double valor)
+        {
+            if(valor < VALOR_MINIMO)
+            {
+                throw new Exception("O valor minimo para saque é R$" + VALOR_MINIMO);
+            }
+            else if (valor > Saldo)
+            {
+               throw new Exception("Saldo insuficiente para saque, seu saldo e R$" + Saldo);
+
+            }
+
+            Saldo -= valor;
+            return valor;
+        }
+
+
+        public void Tranferencia(ContaBancaria contaDestino, double valor)
+        {
+            if(valor < VALOR_MINIMO)
+            {
+                throw new Exception("Valor minimo para tranferencia e de R$" + VALOR_MINIMO);
+            }
+             else if (valor > Saldo)
+            {
+               throw new Exception("Saldo insuficiente para transferencia, seu saldo e R$" + Saldo);
+
+            }
+
+            contaDestino.Depositar(valor);
+        }
+
+        #endregion
     }
 }
