4a3043dc2666148e8e0ee2d5c983e194ae134014
marcosbarker <marcos_bareker@hotmail.com>
2022-04-09 18:02:18 -0300

    code: criateMovimentacao

---
+++ AgenciaBancaria/Enums/TipoMovimentacao.cs
@@ -0,0 +1,10 @@
+namespace AgenciaBancaria.Enums
+{
+    public enum TipoMovimentacao
+    {
+        ABERTURA_CONTA, 
+        SAQUE,
+        DEPOSITO,
+        TRANSFERENCIA
+    }
+}
--- AgenciaBancaria/Program.cs
+++ AgenciaBancaria/Program.cs
@@ -12,6 +12,8 @@ var conta01 = new ContaPoupanca(titular01, 50.0);
 var conta02 = new ContaCorrente(titular02);
 var conta03 = new ConstaInvestimento(titular02);
 
+var movimentacao = new Movimentacao(AgenciaBancaria.Enums.TipoMovimentacao.DEPOSITO, 10);
+
 try
 {
     var valor = conta01.Sacar(10);
@@ -35,4 +37,6 @@ conta02.ImprimirExtrato();
 Console.WriteLine();
 Console.WriteLine("Extrato conta03");
 conta03.ImprimirExtrato();
+Console.WriteLine();
+Console.WriteLine(movimentacao);
 Console.WriteLine();
